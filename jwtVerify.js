bb80fcd99938634a882fac95b8ea475b25155397
ssatyamchauhan <ssatyamchauhan1998@gmail.com>
2019-10-12 14:59:50 +0530

    React-collaborator-todo-back-end

---
+++ jwtVerify.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const config = require('./config')
+
+module.exports = (req,res,next) => {
+
+    var token = req.query.token || req.body.token || req.headers.Cookies;
+    
+    console.log('post request is called ',token)
+    if(token !== undefined){
+        token = token.slice(4,token.length)
+        jwt.verify(token, config.key.secret, (err, decode) =>{
+            if(!err){
+                req.email = decode.user.email 
+                next();
+            }
+            else{
+                console.log(err)
+                res.json('token is expired')
+            }
+        })
+    }
+    else{
+        res.json('please login first!')
+    }
+
+}
+
